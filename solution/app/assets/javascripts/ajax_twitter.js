adb6b02f4d03cf5a23a9215209e751592d6a8208
davidrunger <daverunger@gmail.com>
2015-04-05 17:53:55 -0700

    move plugins from app.js to separate files

---
+++ solution/app/assets/javascripts/ajax_twitter.js
@@ -0,0 +1,6 @@
+$(function () {
+  $("button.follow-toggle").followToggle();
+  $("div.users-search").usersSearch();
+  $("form.tweet-compose").tweetCompose();
+  $("div.infinite-tweets").infiniteTweets();
+});
